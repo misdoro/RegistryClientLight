a04e8773d9ecec3a0571c428a70e5b810441350f
Misha Doronin <misha@doronin.org>
2012-03-30 18:15:20 +0200

    organize imports

--- src/test/java/org/vamdc/registry/search/TestGetResource.java
+++ src/test/java/org/vamdc/registry/search/TestGetResource.java
@@ -4,7 +4,6 @@ import org.vamdc.registry.search.RegistryClientFactory;
 
 import net.ivoa.wsdl.registrysearch.ErrorResp;
 import net.ivoa.wsdl.registrysearch.NotFoundResp;
-import net.ivoa.wsdl.registrysearch.OpUnsupportedResp;
 import net.ivoa.wsdl.registrysearch.RegistrySearchPortType;
 import net.ivoa.xml.voresource.v1.Resource;
 import junit.framework.TestCase;
