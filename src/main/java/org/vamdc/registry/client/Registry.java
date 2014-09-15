aae394f31856f3c8b7125b5625a29efd019923df
Misha Doronin <misha@doronin.org>
2014-09-15 22:44:42 +0200

    Add some javadoc descriptions for the methods to avoid the ambiguities. Deprecate the methods to get access URLs in favor of GetMirrors method.

--- src/main/java/org/vamdc/registry/client/Registry.java
+++ src/main/java/org/vamdc/registry/client/Registry.java
@@ -36,25 +36,63 @@ public interface Registry {
 	
 	
 	/**
-	 * Get all registered IVOA identifiers
+	 * Get all registered IVOA identifiers of the service type Service (VAMDC-TAP or XSAMS-consumer)
 	 * @return a collection of IVOA identifiers from the registry
 	 */
 	public Collection<String> getIVOAIDs(Service standard);
 	
-
-	
+	/**
+	 * Get the capabilities URL of the VAMDC-TAP service ivoaid
+	 * Deprecated - use GetMirrors.
+	 * @param ivoaid
+	 * @return
+	 */
+	@Deprecated
 	public URL getCapabilitiesURL(String ivoaid);
 	
+	/**
+	 * Get the availability URL of the VAMDC-TAP service ivoaid.
+	 * Deprecated - use GetMirrors.
+	 * @param ivoaid
+	 * @return
+	 */
+	@Deprecated
 	public URL getAvailabilityURL(String ivoaid);
 	
+	/**
+	 * Get the complete resource metadata of the registered service ivoaid
+	 * @param ivoaid
+	 * @return
+	 */
 	public Resource getResourceMetadata(String ivoaid);
 	
+	/**
+	 * Get the restrictables of the VAMDC-TAP service ivoaid
+	 * @param ivoaid
+	 * @return
+	 */
 	public Set<Restrictable> getRestrictables(String ivoaid);
 	
+	/**
+	 * Get the preferred processors list of the VAMDC-TAP service ivoaid
+	 * @param ivoaid
+	 * @return
+	 */
 	public List<String> getProcessors(String ivoaid);
 	
+	/**
+	 * Get the VAMDC-TAP URL of the VAMDC-TAP service ivoaid
+	 * @param ivoaid
+	 * @return
+	 */
+	@Deprecated
 	public URL getVamdcTapURL(String ivoaid);
 	
+	/**
+	 * Get all the registered mirrors of the VAMDC-TAP service ivoaid
+	 * @param ivoaid
+	 * @return
+	 */
 	public List<VamdcTapService> getMirrors(String ivoaid);
 	
 }
